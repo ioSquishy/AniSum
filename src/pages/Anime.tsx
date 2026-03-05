b00ec282595c4b09b733e860c60e542c78856e64
ioSquishy <squishylxix@gmail.com>
2026-03-04 23:09:47 -0800

    added anime page and css

--- src/pages/Anime.tsx
+++ src/pages/Anime.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Header, { type HeaderSearchQuery } from "../components/Header";
+import './Anime.css'
 
 interface AnimeInfo {
   id: number,
   defaultTitle: string,
   japaneseTitle: string,
+  imageUrl: string,
   mediaType: string, // TV, Movie, OVA, etc.
   isAiring: boolean,
   seasonAired: string,
@@ -36,10 +38,11 @@ export default function Anime() {
           id: data.data.mal_id,
           defaultTitle: data.data.title,
           japaneseTitle: data.data.title_japanese,
+          imageUrl: data.data.images.webp.large_image_url,
           mediaType: data.data.type,
           isAiring: data.data.airing,
           seasonAired: data.data.season,
-          yearAired: data.data.year,
+          yearAired: data.data.aired.prop.from.year,
           ageRating: data.data.rating,
           synopsis: data.data.synopsis,
           studioNames: data.data.studios.map((studio: { name: string; }) => studio.name),
@@ -59,8 +62,41 @@ export default function Anime() {
   return (
     <>
       <Header onSearch={handleHeaderSearch}/>
-      <div hidden={animeInfo === null} className="anime-details">
-        
+      <div hidden={animeInfo === null} className="info-container">
+        <img src={animeInfo?.imageUrl}></img>
+        <div className="details-container">
+          <div className="titles">
+            <h1>{animeInfo?.defaultTitle}</h1>
+            <h3>{animeInfo?.japaneseTitle}</h3>
+          </div>
+
+          <div className="studios">
+            <h3>Studio{(animeInfo?.studioNames?.length ?? 0) > 1 ? "s" : ""}:</h3>
+            {
+              animeInfo?.studioNames.map(name => <span key={name}>{name}</span>)
+            }
+          </div>
+
+          <div className="tags bubble-wrapper">
+            {
+              animeInfo?.genres.map(name => <span className="bubble" key={name}>{name}</span>)
+            }
+            {
+              animeInfo?.genres.map(name => <span className="bubble" key={name}>{name}</span>)
+            }
+          </div>
+
+          <div className="airing-info bubble-wrapper">
+              <span className="bubble">{animeInfo?.mediaType}</span>
+              <span className="bubble">{animeInfo?.isAiring ? "Airing" : `Aired ${animeInfo?.seasonAired?.toUpperCase() ?? ""} ${animeInfo?.yearAired}`}</span>
+              <span className="bubble">{animeInfo?.ageRating}</span>
+            </div>
+
+          <div>
+            <h2>Synopsis</h2>
+            <blockquote>{animeInfo?.synopsis}</blockquote>
+          </div>
+        </div>
       </div>
       <p hidden={animeInfo !== null || failedLoad}>loading...</p>
       <p hidden={!failedLoad}>Failed to find anime with ID {animeId} :{'('}</p>
